fcbdaab256bd8934360d6366c23d2615953ac593
Ali Sobhani <acdota@outlook.com>
2023-08-28 13:16:18 +0330

    Add project files.

---
+++ KoftePrinter/BarcodeForm.Designer.cs
@@ -0,0 +1,225 @@
+﻿
+namespace KoftePrinter
+{
+    partial class BarcodeForm
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.saveButton = new System.Windows.Forms.Button();
+            this.groupBox1 = new System.Windows.Forms.GroupBox();
+            this.numericUpDown2 = new System.Windows.Forms.NumericUpDown();
+            this.numericUpDown1 = new System.Windows.Forms.NumericUpDown();
+            this.button3 = new System.Windows.Forms.Button();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label1 = new System.Windows.Forms.Label();
+            this.barcode = new System.Windows.Forms.PictureBox();
+            this.printDoc = new System.Drawing.Printing.PrintDocument();
+            this.errorProvider1 = new System.Windows.Forms.ErrorProvider(this.components);
+            this.metroLabel1 = new MetroFramework.Controls.MetroLabel();
+            this.textBoxCode = new MetroFramework.Controls.MetroTextBox();
+            this.metroLabel2 = new MetroFramework.Controls.MetroLabel();
+            this.barcodeFormat = new System.Windows.Forms.ListBox();
+            this.groupBox1.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.numericUpDown2)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.numericUpDown1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.barcode)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.errorProvider1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // saveButton
+            // 
+            this.saveButton.Location = new System.Drawing.Point(604, 44);
+            this.saveButton.Name = "saveButton";
+            this.saveButton.Size = new System.Drawing.Size(114, 32);
+            this.saveButton.TabIndex = 10;
+            this.saveButton.Text = "Save";
+            this.saveButton.UseVisualStyleBackColor = true;
+            this.saveButton.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // groupBox1
+            // 
+            this.groupBox1.Controls.Add(this.numericUpDown2);
+            this.groupBox1.Controls.Add(this.numericUpDown1);
+            this.groupBox1.Controls.Add(this.button3);
+            this.groupBox1.Controls.Add(this.saveButton);
+            this.groupBox1.Controls.Add(this.label2);
+            this.groupBox1.Controls.Add(this.label1);
+            this.groupBox1.Location = new System.Drawing.Point(32, 224);
+            this.groupBox1.Name = "groupBox1";
+            this.groupBox1.Size = new System.Drawing.Size(751, 98);
+            this.groupBox1.TabIndex = 11;
+            this.groupBox1.TabStop = false;
+            this.groupBox1.Text = "Barcode Size";
+            // 
+            // numericUpDown2
+            // 
+            this.numericUpDown2.Location = new System.Drawing.Point(9, 55);
+            this.numericUpDown2.Name = "numericUpDown2";
+            this.numericUpDown2.Size = new System.Drawing.Size(120, 22);
+            this.numericUpDown2.TabIndex = 16;
+            this.numericUpDown2.ValueChanged += new System.EventHandler(this.numericUpDown2_ValueChanged);
+            // 
+            // numericUpDown1
+            // 
+            this.numericUpDown1.Location = new System.Drawing.Point(199, 55);
+            this.numericUpDown1.Name = "numericUpDown1";
+            this.numericUpDown1.Size = new System.Drawing.Size(120, 22);
+            this.numericUpDown1.TabIndex = 15;
+            this.numericUpDown1.ValueChanged += new System.EventHandler(this.numericUpDown1_ValueChanged);
+            // 
+            // button3
+            // 
+            this.button3.Location = new System.Drawing.Point(445, 44);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(114, 32);
+            this.button3.TabIndex = 14;
+            this.button3.Text = "Generate";
+            this.button3.UseVisualStyleBackColor = true;
+            this.button3.Click += new System.EventHandler(this.button3_Click);
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(236, 34);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(49, 17);
+            this.label2.TabIndex = 1;
+            this.label2.Text = "Height";
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(42, 34);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(44, 17);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Width";
+            // 
+            // barcode
+            // 
+            this.barcode.Location = new System.Drawing.Point(180, 63);
+            this.barcode.Name = "barcode";
+            this.barcode.Size = new System.Drawing.Size(603, 142);
+            this.barcode.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.barcode.TabIndex = 12;
+            this.barcode.TabStop = false;
+            this.barcode.Click += new System.EventHandler(this.barcode_Click);
+            // 
+            // printDoc
+            // 
+            this.printDoc.PrintPage += new System.Drawing.Printing.PrintPageEventHandler(this.printDocument1_PrintPage);
+            // 
+            // errorProvider1
+            // 
+            this.errorProvider1.ContainerControl = this;
+            // 
+            // metroLabel1
+            // 
+            this.metroLabel1.AutoSize = true;
+            this.metroLabel1.Location = new System.Drawing.Point(15, 74);
+            this.metroLabel1.Name = "metroLabel1";
+            this.metroLabel1.Size = new System.Drawing.Size(146, 20);
+            this.metroLabel1.TabIndex = 13;
+            this.metroLabel1.Text = "Enter Code to Encode";
+            // 
+            // textBoxCode
+            // 
+            this.textBoxCode.Location = new System.Drawing.Point(15, 97);
+            this.textBoxCode.Name = "textBoxCode";
+            this.textBoxCode.Size = new System.Drawing.Size(146, 23);
+            this.textBoxCode.TabIndex = 14;
+            this.textBoxCode.Text = "TextOrNumber";
+            // 
+            // metroLabel2
+            // 
+            this.metroLabel2.AutoSize = true;
+            this.metroLabel2.Location = new System.Drawing.Point(15, 133);
+            this.metroLabel2.Name = "metroLabel2";
+            this.metroLabel2.Size = new System.Drawing.Size(52, 20);
+            this.metroLabel2.TabIndex = 15;
+            this.metroLabel2.Text = "Format";
+            // 
+            // barcodeFormat
+            // 
+            this.barcodeFormat.FormattingEnabled = true;
+            this.barcodeFormat.ItemHeight = 16;
+            this.barcodeFormat.Items.AddRange(new object[] {
+            "QR-Code",
+            "Code128",
+            "Data Matrix",
+            "Aztec",
+            "PDF 417"});
+            this.barcodeFormat.Location = new System.Drawing.Point(15, 156);
+            this.barcodeFormat.Name = "barcodeFormat";
+            this.barcodeFormat.Size = new System.Drawing.Size(146, 20);
+            this.barcodeFormat.TabIndex = 16;
+            this.barcodeFormat.SelectedIndexChanged += new System.EventHandler(this.listBox1_SelectedIndexChanged);
+            // 
+            // BarcodeForm
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 330);
+            this.Controls.Add(this.barcodeFormat);
+            this.Controls.Add(this.metroLabel2);
+            this.Controls.Add(this.textBoxCode);
+            this.Controls.Add(this.metroLabel1);
+            this.Controls.Add(this.barcode);
+            this.Controls.Add(this.groupBox1);
+            this.Name = "BarcodeForm";
+            this.Text = "Barcode Printer";
+            this.Load += new System.EventHandler(this.BarcodeForm_Load);
+            this.groupBox1.ResumeLayout(false);
+            this.groupBox1.PerformLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.numericUpDown2)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.numericUpDown1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.barcode)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.errorProvider1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+        private System.Windows.Forms.Button saveButton;
+        private System.Windows.Forms.GroupBox groupBox1;
+        private System.Windows.Forms.Button button3;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.PictureBox barcode;
+        private System.Drawing.Printing.PrintDocument printDoc;
+        private System.Windows.Forms.ErrorProvider errorProvider1;
+        private System.Windows.Forms.NumericUpDown numericUpDown1;
+        private System.Windows.Forms.NumericUpDown numericUpDown2;
+        private MetroFramework.Controls.MetroTextBox textBoxCode;
+        private MetroFramework.Controls.MetroLabel metroLabel1;
+        private System.Windows.Forms.ListBox barcodeFormat;
+        private MetroFramework.Controls.MetroLabel metroLabel2;
+    }
+}
+
---
+++ KoftePrinter/BarcodeForm.cs
@@ -0,0 +1,82 @@
+﻿using System;
+using System.IO;
+using System.Windows.Forms;
+using ZXing;
+
+namespace KoftePrinter
+{
+    public partial class BarcodeForm : MetroFramework.Forms.MetroForm
+    {
+
+        BarcodeWriter writer = new BarcodeWriter() { Format = BarcodeFormat.QR_CODE };
+
+        public BarcodeForm()
+        {
+            InitializeComponent();
+        }
+
+        private void Encode_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void label1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void printDocument1_PrintPage(object sender, System.Drawing.Printing.PrintPageEventArgs e)
+        {
+
+        }
+
+        private void BarcodeForm_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            barcode.Image = writer.Write(textBoxCode.Text);
+            
+        }
+
+        private void barcode_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void numericUpDown1_ValueChanged(object sender, EventArgs e)
+        {
+            writer.Options.Width = (int)numericUpDown1.Value;
+        }
+
+        private void numericUpDown2_ValueChanged(object sender, EventArgs e)
+        {
+            writer.Options.Height = (int)numericUpDown2.Value;
+        }
+
+        private void listBox1_SelectedIndexChanged(object sender, EventArgs e)
+        {
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            if (barcode.Image == null)
+            {
+                return;
+            }
+            using(SaveFileDialog savefile = new SaveFileDialog()
+            {
+                Filter = "PNG|*.png"
+            })
+            {
+                if (savefile.ShowDialog() == DialogResult.OK)
+                {
+                    barcode.Image.Save(savefile.FileName);
+                }
+            }
+            //printDoc = writer.Write(textBoxCode.Text);
+        }
+    }
+}
